c996860f9f55efed564cc84d656e5394ebf52fc5
Nate Radebaugh <nate.radebaugh@outlook.com>
2019-09-09 17:04:34 -0500

    Simplify popover implementation and update names.

--- packages/popover/examples/basic.example.js
+++ packages/popover/examples/basic.example.js
@@ -1,26 +1,23 @@
 import React, { useRef, useState } from "react";
-import Popover, { positionDefault, positionMatchWidth } from "../src/index";
+import Popover from "../src/index";
 
 export let name = "Basic";
 
 export function Example() {
-  const ref = useRef();
-  const [value, setValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const targetRef = useRef();
+
   return (
     <div>
       <textarea placeholder="resize me to move stuff around" />
-      <textarea
-        placeholder="Try typing 'match width'"
-        ref={ref}
-        onChange={event => setValue(event.target.value)}
+
+      <input
+        ref={targetRef}
+        onFocus={() => setIsOpen(true)}
+        onBlur={() => setIsOpen(false)}
       />
-      {value.length > 0 && (
-        <Popover
-          targetRef={ref}
-          position={
-            value === "match width" ? positionMatchWidth : positionDefault
-          }
-        >
+      {isOpen && (
+        <Popover targetRef={targetRef}>
           <div
             style={{
               border: "solid 1px",
@@ -30,15 +27,9 @@ export function Example() {
             }}
           >
             <p>Huzzah!!! I am here! WASSUPPPPP</p>
-            <p>
-              Tab navigation from the textarea that triggered this should now
-              move to the button below.
-            </p>
-            <button>I should be the next tab</button>
           </div>
         </Popover>
       )}
-      <button>and then tab to me after that one</button>
     </div>
   );
 }
--- packages/popover/src/index.js
+++ packages/popover/src/index.js
@@ -1,45 +1,6 @@
-import React, { useRef, forwardRef, useEffect } from "react";
-import Portal from "@reach/portal";
+import React, { useRef, forwardRef } from "react";
 import { useRect } from "@reach/rect";
 import { assignRef } from "@reach/utils";
-import tabbable from "tabbable";
-
-export default forwardRef(function Popover(props, ref) {
-  return (
-    <Portal>
-      <PopoverImpl ref={ref} {...props} />
-    </Portal>
-  );
-});
-
-// Popover is conditionally rendered so we can't start measuring until it shows
-// up, so useRect needs to live down here not up in Popover
-const PopoverImpl = forwardRef(function PopoverImpl(
-  { targetRef, position = positionDefault, style, ...rest },
-  forwardedRef
-) {
-  const popoverRef = useRef();
-  const popoverRect = useRect(popoverRef);
-  const targetRect = useRect(targetRef);
-
-  useSimulateTabNavigationForReactTree(targetRef, popoverRef);
-
-  return (
-    <div
-      data-reach-popover=""
-      ref={node => {
-        assignRef(popoverRef, node);
-        assignRef(forwardedRef, node);
-      }}
-      style={{
-        ...style,
-        position: "absolute",
-        ...getStyles(position, targetRect, popoverRect)
-      }}
-      {...rest}
-    />
-  );
-});
 
 const getStyles = (position, targetRect, popoverRect) => {
   const needToMeasurePopup = !popoverRect;
@@ -49,7 +10,7 @@ const getStyles = (position, targetRect, popoverRect) => {
   return position(targetRect, popoverRect);
 };
 
-export function positionDefault(targetRect, popoverRect) {
+function positionDefault(targetRect, popoverRect) {
   const { directionUp, directionRight } = getCollisions(
     targetRect,
     popoverRect
@@ -63,36 +24,6 @@ export function positionDefault(targetRect, popoverRect) {
       : `${targetRect.top + targetRect.height + window.pageYOffset}px`
   };
 }
-
-export function positionMatchWidth(targetRect, popoverRect) {
-  const { directionUp } = getCollisions(targetRect, popoverRect);
-  return {
-    width: targetRect.width,
-    left: targetRect.left,
-    top: directionUp
-      ? `${targetRect.top - popoverRect.height + window.pageYOffset}px`
-      : `${targetRect.top + targetRect.height + window.pageYOffset}px`
-  };
-}
-
-// Finish this another time
-// export function positionHorizontalCenter(targetRect, popoverRect) {
-//   const targetCenter = targetRect.width / 2 + targetRect.left;
-//   const popoverHalf = popoverRect.width / 2;
-
-//   const collisions = {
-//     right: window.innerWidth < targetCenter - popoverHalf,
-//     left: targetCenter - popoverHalf < 0
-//     // top:
-//     // bottom:
-//   };
-
-//   return {
-//     left: collisions.right
-//       ? `${targetRect.right - popoverRect.width + window.pageXOffset}px`
-//       : collisions.left ? `` : ``
-//   };
-// }
 
 function getCollisions(
   targetRect,
@@ -115,125 +46,29 @@ function getCollisions(
   return { directionRight, directionUp };
 }
 
-// Heads up, my jQuery past haunts this function. This hook scopes the tab
-// order to the React element tree, instead of the DOM tree. This way, when the
-// user navigates with tab from the targetRef, the tab order moves into the
-// popup, and then out of the popup back to the rest of the document.
-// (We call targetRef, triggerRef inside this function to avoid confusion with
-// event.target)
-function useSimulateTabNavigationForReactTree(triggerRef, popoverRef) {
-  const doc = triggerRef.current.ownerDocument; // maybe in devtools
+const Popover = forwardRef(function Popover(
+  { targetRef, position = positionDefault, style, ...rest },
+  forwardedRef
+) {
+  const popoverRef = useRef();
+  const popoverRect = useRect(popoverRef);
+  const targetRect = useRect(targetRef);
 
-  function handleKeyDown(event) {
-    if (event.key === "Tab" && tabbable(popoverRef.current).length === 0) {
-      return;
-    }
+  return (
+    <div
+      data-reach-popover=""
+      ref={node => {
+        assignRef(popoverRef, node);
+        assignRef(forwardedRef, node);
+      }}
+      style={{
+        ...style,
+        position: "absolute",
+        ...getStyles(position, targetRect, popoverRect)
+      }}
+      {...rest}
+    />
+  );
+});
 
-    if (event.key === "Tab" && event.shiftKey) {
-      if (shiftTabbedFromElementAfterTrigger(event)) {
-        focusLastTabbableInPopover(event);
-      } else if (shiftTabbedOutOfPopover(event)) {
-        focusTriggerRef(event);
-      } else if (shiftTabbedToBrowserChrome(event)) {
-        disableTabbablesInPopover(event);
-      }
-    } else if (event.key === "Tab") {
-      if (tabbedFromTriggerToPopover(event)) {
-        focusFirstPopoverTabbable(event);
-      } else if (tabbedOutOfPopover(event)) {
-        focusTabbableAfterTrigger(event);
-      } else if (tabbedToBrowserChrome(event)) {
-        disableTabbablesInPopover(event);
-      }
-    }
-  }
-
-  useEffect(() => {
-    doc.addEventListener("keydown", handleKeyDown);
-    return () => doc.removeEventListener("keydown", handleKeyDown);
-  }, []);
-
-  function getElementAfterTrigger() {
-    const elements = tabbable(doc);
-    const targetIndex = elements.indexOf(triggerRef.current);
-    return elements[targetIndex + 1];
-  }
-
-  function tabbedFromTriggerToPopover() {
-    return triggerRef.current === document.activeElement;
-  }
-  function focusFirstPopoverTabbable(event) {
-    const elements = tabbable(popoverRef.current);
-    if (elements[0]) {
-      event.preventDefault();
-      elements[0].focus();
-    }
-  }
-
-  function tabbedOutOfPopover(event) {
-    const inPopover = popoverRef.current.contains(document.activeElement);
-    if (inPopover) {
-      const elements = tabbable(popoverRef.current);
-      return elements[elements.length - 1] === document.activeElement;
-    }
-  }
-  function focusTabbableAfterTrigger(event) {
-    const elementAfterTrigger = getElementAfterTrigger();
-    if (elementAfterTrigger) {
-      event.preventDefault();
-      elementAfterTrigger.focus();
-    }
-  }
-
-  function shiftTabbedFromElementAfterTrigger(event) {
-    if (!event.shiftKey) return;
-    const elementAfterTrigger = getElementAfterTrigger();
-    return event.target === elementAfterTrigger;
-  }
-  function focusLastTabbableInPopover(event) {
-    const elements = tabbable(popoverRef.current);
-    const last = elements[elements.length - 1];
-    if (last) {
-      event.preventDefault();
-      last.focus();
-    }
-  }
-
-  function shiftTabbedOutOfPopover(event) {
-    const elements = tabbable(popoverRef.current);
-    return elements.length === 0 ? false : event.target === elements[0];
-  }
-  function focusTriggerRef(event) {
-    event.preventDefault();
-    triggerRef.current.focus();
-  }
-
-  function tabbedToBrowserChrome(event) {
-    const elements = tabbable(doc).filter(
-      element => !popoverRef.current.contains(element)
-    );
-    return event.target === elements[elements.length - 1];
-  }
-
-  function shiftTabbedToBrowserChrome(event) {
-    // we're assuming the popover will never contain the first tabbable
-    // element, and it better not, because the trigger needs to be tabbable!
-    return event.target === tabbable(doc)[0];
-  }
-
-  let restoreTabIndexTuplés = [];
-  function disableTabbablesInPopover() {
-    const elements = tabbable(popoverRef.current);
-    elements.forEach(element => {
-      restoreTabIndexTuplés.push([element, element.tabIndex]);
-      element.tabIndex = -1;
-    });
-    doc.addEventListener("focusin", enableTabbablesInPopover);
-  }
-  function enableTabbablesInPopover(event) {
-    doc.removeEventListener("focusin", enableTabbablesInPopover);
-    restoreTabIndexTuplés.forEach(([element, tabIndex]) => {
-      element.tabIndex = tabIndex;
-    });
-  }
-}
+export default Popover;
